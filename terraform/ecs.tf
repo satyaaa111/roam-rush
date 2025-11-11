0a39ebcbb61e257feaf05b95de56dccc7467912f
satyaaa111 <juman60000@gmail.com>
2025-11-11 23:51:14 +0530

    random fix

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -6,6 +6,8 @@ resource "aws_ecs_cluster" "main" {
 # --- 2a. IAM Role for Fargate ---
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "roamrush-ecs-task-exec-role-${terraform.workspace}"
+  
+  # This "Trust Relationship" allows ECS to assume this role
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -16,28 +18,60 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
-# --- 2b. Attach Default ECS Policy ---
-# (For ECR logs)
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_attachment" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+# --- 2b. The *Correct* IAM Policy (Permissions) ---
+# This policy grants the 3 permissions our container *actually* needs
+data "aws_iam_policy_document" "ecs_task_exec_policy_doc" {
+  # ECR permissions
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage"
+    ]
+    resources = ["*"]
+  }
+  
+  # CloudWatch Logs permissions
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      aws_cloudwatch_log_group.backend_logs.arn,
+      aws_cloudwatch_log_group.frontend_logs.arn,
+      "${aws_cloudwatch_log_group.backend_logs.arn}:*",
+      "${aws_cloudwatch_log_group.frontend_logs.arn}:*"
+    ]
+  }
+
+  # Secrets Manager permissions
+  statement {
+    effect  = "Allow"
+    actions = ["secretsmanager:GetSecretValue"]
+    resources = [
+      aws_secretsmanager_secret.postgres.arn,
+      aws_secretsmanager_secret.mongo.arn,
+      aws_secretsmanager_secret.jwt_secret.arn
+    ]
+  }
 }
 
-# --- 2c. THE FIX: Attach Secrets Manager Policy ---
-resource "aws_iam_role_policy_attachment" "ecs_secrets_attachment" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  # This policy allows "secretsmanager:GetSecretValue"
-  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite" 
+# Creates the policy in AWS
+resource "aws_iam_policy" "ecs_task_exec_policy" {
+  name   = "roamrush-ecs-task-exec-policy-${terraform.workspace}"
+  policy = data.aws_iam_policy_document.ecs_task_exec_policy_doc.json
 }
 
-# --- 2d. THE FIX: Attach SSM Policy ---
-resource "aws_iam_role_policy_attachment" "ecs_ssm_attachment" {
+# Attaches our new custom policy to our ECS role
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_policy_attachment" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  # This policy allows "ssm:GetParameters"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+  policy_arn = aws_iam_policy.ecs_task_exec_policy.arn
 }
 # ----------------- END OF FIX -----------------
-
 
 # --- 3. The PUBLIC Load Balancer (for Frontend) ---
 resource "aws_lb" "public" {
@@ -88,7 +122,7 @@ resource "aws_lb_target_group" "backend" {
   target_type = "ip"
   
   health_check {
-    path = "/api/v1/test"
+    path = "/actuator/health"
   }
 }
 
