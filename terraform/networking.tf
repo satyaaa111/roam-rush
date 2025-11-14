2e331b72f283a8cd446f3e65b00bc65e38b8c77c
satyaaa111 <juman60000@gmail.com>
2025-11-14 05:39:25 +0530

    changes made to allow traffic from ipv6

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -19,11 +19,23 @@ resource "aws_security_group" "alb_public" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
 
